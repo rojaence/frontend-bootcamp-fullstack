026aa2a696c0e402f666d88cb6e5be641a6dc7d5
Ronny Endara <rojaence@gmail.com>
2025-02-03 12:09:41 -0500

    Agregado: Diseño y validaciones de pagina de login con bulmacss

---
+++ 04-html-css-exercises/js/bulma/login.js
@@ -0,0 +1,57 @@
+import { AuthService } from '../authService.js'
+
+const loginForm = document.getElementById('login-form')
+const passwordInput = document.getElementById('user-password')
+const emailInput = document.getElementById('user-email')
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (!loginForm.checkValidity()) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  const form = new FormData(e.target)
+  const email = form.get('email')
+  const password = form.get('password')
+
+  const validation = AuthService.validate(email, password)
+  setEmailInputError()
+  setPasswordInputError()
+  if (validation.success) {
+    window.location.href = 'http://127.0.0.1:5500/04-html-css-exercises/pages/dashboard.html'
+  }
+})
+
+document.querySelector('#user-password').addEventListener('input', (e) => {
+  setPasswordInputError()
+})
+document.querySelector('#user-email').addEventListener('input', (e) => {
+  setEmailInputError()
+})
+
+const setPasswordInputError = () => {
+  const validation = AuthService.validate(emailInput.value, passwordInput.value)
+  const passwordField = loginForm.querySelector('#password-field')
+  if (validation.errors.password) {
+    passwordField.querySelector('.invalid-feedback').textContent = validation.errors.password
+    passwordField.querySelector('input').classList.add('is-danger')
+    passwordField.querySelector('.invalid-feedback').classList.remove('is-hidden')
+  } else {
+    console.log('vlido')
+    passwordField.querySelector('.invalid-feedback').classList.add('is-hidden')
+    passwordField.querySelector('input').classList.remove('is-danger')
+  }
+}
+
+const setEmailInputError = () => {
+  const validation = AuthService.validate(emailInput.value, passwordInput.value)
+  const emailField = loginForm.querySelector('#email-field')
+  if (validation.errors.email) {
+    emailField.querySelector('.invalid-feedback').textContent = validation.errors.email
+    emailField.querySelector('.invalid-feedback').classList.remove('is-hidden')
+    emailField.querySelector('input').classList.add('is-danger')
+  } else {
+    emailField.querySelector('input').classList.remove('is-danger')
+    emailField.querySelector('.invalid-feedback').classList.add('is-hidden')
+  }
+}
